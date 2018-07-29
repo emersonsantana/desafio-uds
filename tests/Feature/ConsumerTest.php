b4b1c92d928cfaf4a815fe1323785e67e8ce01fe
emersonsantanac <e.santanacunha96@gmail.com>
2018-07-29 16:53:56 -0300

    Novos Testes para Produto e Pessoa (Product e Consumers)

--- tests/Feature/ConsumerTest.php
+++ tests/Feature/ConsumerTest.php
@@ -10,12 +10,14 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class ConsumerTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Teste Pessoa - Consumer.
      *
      * @return void
      */
      private $cpf = '60416272088';
-
+ /*
+ *  Afirme que a resposta tem um determinado código:
+ */
     public function testGetConsumers()
     {
         $response = $this->get('/api/consumers');
@@ -37,6 +39,53 @@ class ConsumerTest extends TestCase
     public function testRemoveConsumer()
     {
       $response = $this->json('Delete', 'api/consumers/'.$this->cpf);
-      $response->assertStatus(200);
+      $response->assertStatus(202);
+    }
+
+/*
+ *  Afirme que uma tabela no banco de dados não contém os dados fornecidos.
+ *  É passado um CPF inválido '123'. A aplicação não pode inserir tal registro.
+ */
+    public function testStoreConsumerCPFFail()
+    {
+        $response = $this->json('Post','/api/consumers',
+          [
+            "cpf" => 123,
+            "name" => "Consumer Test CPF ERROR",
+            "birth_date" => "1980/01/01"
+          ]
+        );
+          $this->assertDatabaseMissing('consumers', [
+            'name' => 'Consumer Test CPF ERROR'
+        ]);
+    }
+
+/*
+ *  Afirme que uma tabela no banco de dados contém os dados fornecidos.
+ *  Todos os dados são válidos. A aplicação deve persistir tal registro.
+ */
+    public function testStoreConsumerBD()
+    {
+      $response = $this->json('Post','/api/consumers',
+        [
+          "cpf" => $this->cpf,
+          "name" => "Consumer Test OK",
+          "birth_date" => "1980/01/01"
+        ]
+      );
+          $this->assertDatabaseHas('consumers', [
+            'name' => 'Consumer Test OK'
+        ]);
+    }
+/*
+*  Afirme que uma tabela no banco de dados não contém os dados fornecidos.
+*  Faz a Remoção de um registro recém inserido. A aplicação deve remover o Registro com sucesso.
+*/
+    public function testRemoveConsumerBD()
+    {
+      $response = $this->json('Delete', 'api/consumers/'.$this->cpf);
+          $this->assertDatabaseMissing('consumers', [
+            'name' => 'Consumer Test OK'
+        ]);
     }
 }
--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -6,16 +6,19 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Http\Repositories\ProductRepository;
+
 
 class ProductTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
+     *   Teste Produto (product)
      * @return void
      */
      private $code = '10';
-
+/*
+*  Afirme que a resposta tem um determinado código:
+*/
     public function testGetProducts()
     {
         $response = $this->get('/api/products');
@@ -31,13 +34,59 @@ class ProductTest extends TestCase
         "price" => "29.90"
       ]
     );
-    $response->assertStatus(201);
+      $response->assertStatus(201);
     }
 
     public function testRemoveProduct()
     {
       $response = $this->json('Delete', 'api/products/'.$this->code);
-      $response->assertStatus(200);
+      $response->assertStatus(202);
     }
+/*
+ *  Afirme que uma tabela no banco de dados não contém os dados fornecidos.
+ *  É passado um Valor inválido pois é negativo. A aplicação não pode inserir tal registro.
+ */
+    public function testStoreProductPriceFail()
+    {
+        $response = $this->json('Post','/api/products',
+          [
+            "code" => $this->code,
+            "name" => "Product Test Price",
+            "price" => "-10"
+          ]
+        );
+          $this->assertDatabaseMissing('products', [
+            'name' => 'Product Test Price'
+        ]);
+    }
+/*
+ *  Afirme que uma tabela no banco de dados contém os dados fornecidos.
+ *  Todos os dados são válidos. A aplicação deve persistir tal registro.
+ */
+    public function testStoreProductBD()
+    {
+        $response = $this->json('Post','/api/products',
+          [
+            "code" => $this->code,
+            "name" => "Product Test Best",
+            "price" => "100"
+          ]
+        );
+          $this->assertDatabaseHas('products', [
+            'name' => 'Product Test Best'
+        ]);
+    }
+/*
+ *  Afirme que uma tabela no banco de dados não contém os dados fornecidos.
+ *  Faz a Remoção de um registro recém inserido. A aplicação deve remover o Registro com sucesso.
+ */
+    public function testRemoveProductBD()
+    {
+      $response = $this->json('Delete', 'api/products/'.$this->code);
+          $this->assertDatabaseMissing('products', [
+            'name' => 'Product Test Best'
+        ]);
+    }
+
 
 }
